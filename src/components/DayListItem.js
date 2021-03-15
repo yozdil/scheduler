c8861612d459fd6f62cef9ffe3d25c9722550967
Yamac Ozdil <yamac.ozdil@outlook.com>
2021-03-15 17:10:54 -0400

    W07D1

---
+++ src/components/DayListItem.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+import "components/DayListItem.scss";
+
+import classNames from "classnames/bind";
+
+export default function DayListItem({ spots, selected, name, setDay }) {
+  const formatSpots = () => {
+    if (!spots) {
+      return "no spots remaining";
+    } else if (spots === 1) {
+      return `${spots} spot remaining`;
+    } else {
+      return `${spots} spots remaining`;
+    }
+  };
+  const dayClass = classNames({
+    "day-list__item": true,
+    "day-list__item--full": !spots,
+    "day-list__item--selected": selected,
+  });
+
+  return (
+    <li className={dayClass} onClick={() => setDay(name)}>
+      <h2 className="text--regular">{name}</h2>
+      <h3 className="text--light">{formatSpots()}</h3>
+    </li>
+  );
+}
