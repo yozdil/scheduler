31337e3488120f498af6735a272b1d1f1cfddb94
Yamac Ozdil <yamac.ozdil@outlook.com>
2021-03-24 12:52:26 -0400

    import from axios

--- src/index.js
+++ src/index.js
@@ -4,10 +4,10 @@ import Application from "components/Application";
 import "index.scss";
 
 
-// import axios from "axios";
-// if (process.env.REACT_APP_API_BASE_URL) {
-//   axios.defaults.baseURL = process.env.REACT_APP_API_BASE_URL;
-// }
+import axios from "axios";
+if (process.env.REACT_APP_API_BASE_URL) {
+  axios.defaults.baseURL = process.env.REACT_APP_API_BASE_URL;
+}
 
 
 
